658fc25f99cd149b6848bd7336bef5907e960f8e
Ameer Jhan <ameer.jhan@olxpeople.com>
2021-02-20 16:43:35 +0530

    ref: support blazepack

---
+++ packages/app/src/sandbox/build-blazepack-core.sh
@@ -0,0 +1,4 @@
+yarn build:sandpack-sandbox
+cp ../../../app/static/js/babel.7.12.12.min.js ../../www/static/js/
+mkdir ../../www/static/browserfs12
+cp ../../../../standalone-packages/codesandbox-browserfs/dist/browserfs.min.js ../../www/static/browserfs12/
